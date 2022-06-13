f91b6023654e59c1236c547c4cf4ed9aeb5426f1
geombong <shoy1415@gmail.com>
2022-06-14 00:24:48 +0900

    Event domain

---
+++ src/main/java/events/Event.java
@@ -0,0 +1,33 @@
+package events;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Event {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollMentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location;
+    private int basePrice;
+    private int maxPrice;
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+}
---
+++ src/test/java/events/EventTest.java
@@ -0,0 +1,42 @@
+package events;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class EventTest {
+
+    @Test
+    @DisplayName("event 클래스에 빌더가 존재하는 경우 테스트가 통과해야 한다.")
+    void eventBuilderTest() {
+        //given
+        //when
+        Event event = Event.builder()
+                .name("Spring REST API")
+                .description("Study")
+                .build();
+        //then
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    @DisplayName("빌더를 사용하지 않고 클래스 생성이 가능한 경우 테스트가 통과해야 한다.")
+    void javaBeanTest() {
+        //given
+        String name = "Event";
+        String description = "Spring";
+
+        //when
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        //then
+        assertSoftly(softAssertions -> {
+            softAssertions.assertThat(event.getName()).as("이벤트 클래스의 이름은 Event 여야 한다.").isEqualTo(name);
+            softAssertions.assertThat(event.getDescription()).as("이벤트 클래스의 설명은 Spring 이여야 한다.").isEqualTo(description);
+        });
+    }
+}
