015f69e05f497fa893dc5cd4f96b3b614ab44d98
geombong <shoy1415@gmail.com>
2022-06-28 23:07:17 +0900

    Builder 변경

--- src/main/java/study/springrestapi/events/EventDto.java
+++ src/main/java/study/springrestapi/events/EventDto.java
@@ -2,15 +2,14 @@ package study.springrestapi.events;
 
 import java.time.LocalDateTime;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EventDto {
 
     private String name;
@@ -22,4 +21,17 @@ public class EventDto {
     private int basePrice;
     private int maxPrice;
     private int limitOfEnrollment;
+
+    @Builder
+    public EventDto(String name, String description, LocalDateTime beginEnrollmentDateTime, LocalDateTime closeEnrollmentDateTime, LocalDateTime endEventDateTime, String location, int basePrice, int maxPrice, int limitOfEnrollment) {
+        this.name = name;
+        this.description = description;
+        this.beginEnrollmentDateTime = beginEnrollmentDateTime;
+        this.closeEnrollmentDateTime = closeEnrollmentDateTime;
+        this.endEventDateTime = endEventDateTime;
+        this.location = location;
+        this.basePrice = basePrice;
+        this.maxPrice = maxPrice;
+        this.limitOfEnrollment = limitOfEnrollment;
+    }
 }
